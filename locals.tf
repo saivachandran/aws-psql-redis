2f2b286592acaf91d713d9cc621c3a77eed99cae
siva <siva1220@gmail.com>
2021-12-03 16:52:42 +0530

    redis added

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   user_data = base64encode(templatefile("${path.module}/userdata.sh.tpl", {
   host  = aws_db_instance.psql-sameed.address
-  username  = var.hostname
-  password  = var.hostname
+  username  = var.username
+  password  = var.password
     }
   ))
 }
