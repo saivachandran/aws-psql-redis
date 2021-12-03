b5589557ed6b6dbb9c542b1b8e0ff8b01e401823
siva <siva1220@gmail.com>
2021-12-03 17:01:54 +0530

    redis added

--- _variable.tf
+++ _variable.tf
@@ -12,7 +12,9 @@ variable "instance_type" {}
 variable "key_name" {}
 variable "additional_tags" { type = map(string) }
 variable "security_group_name" {}
+
 variable "ingress_rule" { type = map(list(string)) }
+
 variable "user_data" {
   type        = string
   description = "if we have user_data, we can update here in the from of bash script"
--- locals.tf
+++ locals.tf
@@ -1,8 +1,9 @@
 locals {
+  host = aws_db_instance.psql-sameed.address
   user_data = base64encode(templatefile("${path.module}/userdata.sh.tpl", {
-  host  = aws_db_instance.psql-sameed.address
-  username  = var.username
-  password  = var.password
+    host  = local.host
+    username  = var.username
+    password  = var.password
     }
   ))
 }
