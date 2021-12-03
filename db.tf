332df3f13e40c0746ab5f241daadc6daa3c01b3d
siva <siva1220@gmail.com>
2021-12-03 16:26:07 +0530

    modified

--- db.tf
+++ db.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "_" {
   name       = "psql-subnet-group"
-  subnet_ids = [aws_subnet.private_subnet_1.id]
+  subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
 }
 
 
