1fbcee903cb7921952c2205fa5249898b2387922
siva <siva1220@gmail.com>
2021-12-04 10:00:43 +0530

    project added

--- infra-code/main.tf
+++ infra-code/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true  
-  tags = tomap(var.additional_tags)
+  tags = {}
 }
 
 # public subnet function
