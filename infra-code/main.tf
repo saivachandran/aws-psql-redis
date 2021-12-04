e97d1e0361f12bafcd1be01d1f69001b7ac7f565
siva <siva1220@gmail.com>
2021-12-04 10:21:51 +0530

    tag added

--- infra-code/main.tf
+++ infra-code/main.tf
@@ -46,7 +46,7 @@ resource "aws_route_table" "main" {
 resource "aws_route_table_association" "internet_access" {
   count          = length(var.availability_zones)
   subnet_id      = aws_subnet.public_subnet[count.index].id
-  route_table_id = aws_route_table.main.id
+  route_table_id = aws_route_table.main[count.index].id
 }
 
 # Private subnet functins
